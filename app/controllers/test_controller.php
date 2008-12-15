a3c2cb2d8147a7d64b0db62456f273ccbcd983f6
unknown <Administrator@.(none)>
2008-12-15 08:34:50 +0800

    remove app/models/model

--- app/controllers/test_controller.php
+++ app/controllers/test_controller.php
@@ -1,6 +1,7 @@
 <?php
 class TestController extends ApplicationController{
 	public function index(){
+		$post = new Post();
 		$this->set('action', __METHOD__);
 	}
 }
--- app/models/model.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
- * Created on Dec 12, 2008
- *
- * To change the template for this generated file go to
- * Window - Preferences - PHPeclipse - PHP - Code Templates
- */
-?>
