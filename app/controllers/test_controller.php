4c2139c66f67fc56bde095d80adaea4e894f6e0e
root <caingougou@gmail.com>
2008-12-31 08:33:10 +0800

    active record updated

--- app/controllers/test_controller.php
+++ app/controllers/test_controller.php
@@ -3,36 +3,34 @@ class TestController extends ApplicationController {
 	public function index() {
 		$this->action = __METHOD__;
 
-//		$user = User :: find(1);
-//		$first_post = Post :: find(1);
-//		pr($user);
-//		pr($first_post);
-//		pr($user->posts[1]->title);
-//		$post = new Post();
-//		$post->title = 'test';
-//		$second_and_third_post = Post::find(array(2,3));
-//		$post_count = Post::count();
-//		$next_post = Post::create(array('title' => 5, 'content' => 6));
-//		$next_post->save();
-//		$next_post->save(array('title' => 5, 'content' => 6);
-//		pr($post);
-//		pr($first_post);
-//		pr($second_post);
-//		pr($third_post);
-//		pr($post_count);
-//		pr($next_post);
-//		$next_post->update(array('content' => 7));
-//		pr($next_post);
-//		$post_count2 = Post::count();
-//		pr($post_count2);
-//		$next_post->delete();
-//		$post_count3 = Post::count();
-//		pr($post_count3);
-//		echo User::count();
-//		if(User::count() == 0) {
-//		$user = new User();
-//		$user->save(array('name' => 'admin', 'password' => md5('admin')));
-//		}
+		//$user = User::model()->find(1);
+		//$post = Post::model()->find(1);
+		//pr($user);
+		//pr($post);
+		//pr($user->posts[0]->title);
+		//pr($user->posts[1]->title);
+		//$post_two = new Post();
+		//$post_two->title = 'test';
+		//pr($post_two);
+		//$second_and_third_post = Post::model()->find(array(2,3));
+		//pr($second_and_third_post);
+		//$post_count = Post::model()->count();
+		//pr($post_count);
+		//$next_post = Post::model()->create(array('title' => 5, 'content' => 6));
+		//var_dump($next_post);
+		//$next_post->save();
+		//$next_post->save(array('title' => 5, 'content' => 6));
+		//$post->update(array('content' => 7));
+		//pr($next_post);
+		//$post_count2 = Post::model()->count();
+		//pr($post_count2);
+		//$next_post->delete();
+		//$post_count3 = Post::model()->count();
+		//pr($post_count3);
+		//echo User::count();
+		//if(User::count() == 0) {
+		//$user = new User();
+		//$user->save(array('name' => 'admin', 'password' => md5('admin')));
 
 //		$this->info("self", __FILE__, __LINE__);
 
