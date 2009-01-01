80503532cfb860dcea432c2d37c2070ce028fa1b
david.yu <test.yu@gmail.com>
2009-01-01 13:37:31 +0800

    test

--- app/controllers/test_controller.php
+++ app/controllers/test_controller.php
@@ -29,46 +29,44 @@ class TestController extends ApplicationController {
 		//pr($post_count3);
 		//echo User::count();
 		//if(User::count() == 0) {
-			//$user = new User();
-			//$user->save(array('name' => 'admin', 'password' => md5('admin')));
+		//$user = new User();
+		//$user->save(array('name' => 'admin', 'password' => md5('admin')));
+		//		$this->info("self", __FILE__, __LINE__);
 
-			//		$this->info("self", __FILE__, __LINE__);
-
-			//		$this->redirect_to("/");
-			//		$this->send_file(__FILE__);
-			//		$this->render("test string render");
-			//		$this->render(array('text' => "test string render"));
-			//		$this->render(array('text' => "test string render", 'layout' => true));
-			//		$this->render(array('layout' => false));
-			//		$this->render(array('layout' => 'posts'));
-			//		$this->render(array('layout' => 'not_exists_layout')); // trigger warning
-			//		$this->render(array('inline' => "render $this->action"));
-			//		$this->render(array('inline' => "render $this->action", 'layout' => false));
-			//		$this->render(array('js' => "alert('__METHOD__')"));
-			//		$this->render(array('json' => "{name:'$this->action'}"));
-			//		$this->render(array('json' => "{name:'$this->action'}", 'callback' => 'show'));
-			//		$this->render(array('nothing' => true));
-			//		$this->render(array('nothing' => false));
-			//		$this->render(array('status' => 202));
-			//		$this->render(array('status' => 202, 'layout' => false));
-			//		$this->render(array('location' => '/', 'status' => 301)); // move permanently redirection 301
-			//		$this->render(array('locals' => array('var1' => 'locals_var1', 'var2' => 'locals_var2')));
-			//		$this->render(array('file' => '/Users/yu/Sites/phpfirefly/app/views/test/test.php'));
-			//		$this->render('/Users/yu/Sites/phpfirefly/app/views/test/test.php');
-			//		$this->render('posts/index');
-			//		$this->render('test');
-			//		$this->render('test2');
-			//		$this->render(array('template' => 'posts/index'));
-			//		$this->render(array('template' => 'posts/index2')); // template not exists.
-			//		$this->render(array('action' => 'posts/index'));
-			//		$this->render(array('action' => 'posts/index', 'layout' => false));
-			//		$this->render(array('action' => 'test'));
-			//		$this->render(array('partial' => 'form'));
-			//		$this->render(array('partial' => 'form', 'layout' => false));
-			//		$this->render(array('partial' => 'posts/form'));
-			//		$this->render(array('partial' => 'posts/form', 'layout' => 'posts'));
-		}
-
+		//		$this->redirect_to("/");
+		//		$this->send_file(__FILE__);
+		//		$this->render("test string render");
+		//		$this->render(array('text' => "test string render"));
+		//		$this->render(array('text' => "test string render", 'layout' => true));
+		//		$this->render(array('layout' => false));
+		//		$this->render(array('layout' => 'posts'));
+		//		$this->render(array('layout' => 'not_exists_layout')); // trigger warning
+		//		$this->render(array('inline' => "render $this->action"));
+		//		$this->render(array('inline' => "render $this->action", 'layout' => false));
+		//		$this->render(array('js' => "alert('__METHOD__')"));
+		//		$this->render(array('json' => "{name:'$this->action'}"));
+		//		$this->render(array('json' => "{name:'$this->action'}", 'callback' => 'show'));
+		//		$this->render(array('nothing' => true));
+		//		$this->render(array('nothing' => false));
+		//		$this->render(array('status' => 202));
+		//		$this->render(array('status' => 202, 'layout' => false));
+		//		$this->render(array('location' => '/', 'status' => 301)); // move permanently redirection 301
+		//		$this->render(array('locals' => array('var1' => 'locals_var1', 'var2' => 'locals_var2')));
+		//		$this->render(array('file' => '/Users/yu/Sites/phpfirefly/app/views/test/test.php'));
+		//		$this->render('/Users/yu/Sites/phpfirefly/app/views/test/test.php');
+		//		$this->render('posts/index');
+		//		$this->render('test');
+		//		$this->render('test2');
+		//		$this->render(array('template' => 'posts/index'));
+		//		$this->render(array('template' => 'posts/index2')); // template not exists.
+		//		$this->render(array('action' => 'posts/index'));
+		//		$this->render(array('action' => 'posts/index', 'layout' => false));
+		//		$this->render(array('action' => 'test'));
+		//		$this->render(array('partial' => 'form'));
+		//		$this->render(array('partial' => 'form', 'layout' => false));
+		//		$this->render(array('partial' => 'posts/form'));
+		//		$this->render(array('partial' => 'posts/form', 'layout' => 'posts'));
 	}
-	?>
 
+}
+?>
